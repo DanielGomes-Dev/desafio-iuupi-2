b339bf71f8edb9c5eac0d7fcc94b94777f22b349
Daniel Gomes <danielgomes1@live.com>
2026-08-08 12:09:35 -0300

    feat: generate login screen

---
+++ carteira_digital_escolar/lib/core/constants/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  static const Color primary = Color(0xFF6C63FF);
+  static const Color background = Color(0xFFF7F7F9);
+  static const Color textPrimary = Color(0xFF1A1A1A);
+  static const Color textSecondary = Color(0xFF8A8A8E);
+  static const Color inputFill = Color(0xFFFFFFFF);
+  static const Color inputBorder = Color(0xFFE5E5EA);
+}
